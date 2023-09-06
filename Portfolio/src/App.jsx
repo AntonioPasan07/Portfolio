4d5c65d1263afd444ba35277e948e68c71ed2995
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-09-06 17:23:50 +0200

    (feat), add the file with the components

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,21 @@
-
-
-
+import Navbar from "./components/Navbar/index"
+import Header from './components/Header/index'
+import Profile from "./components/Profile"
+import About from './components/About'
+import CardContainer from "./components/CardContainer"
+import EmailContact from "./components/Contact"
 function App() {
   
 
-  return (
-    <h1>Hola mundo</h1>
+  return (<>
+    <Header/>
+    <Profile/>
+    <About/>
+    <CardContainer/>
+    <Navbar/>
+    <EmailContact/>
+   
+    </>
   )
    
 }
