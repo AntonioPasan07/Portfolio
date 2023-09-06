7d5c14a82f487f61a9da6019573a3094f04be00f
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-09-06 17:11:59 +0200

    (feat), component to create the form with react hook form for email

---
+++ Portfolio/src/components/Contact/index.jsx
@@ -0,0 +1,122 @@
+import { useForm } from 'react-hook-form'
+import Styled from './styles'
+import React from 'react';
+import emailjs from '@emailjs/browser'
+
+const EmailContact = () => {
+  const SERVICE = import.meta.env.VITE_SERVICES_ID
+  const TEMPLATE = import.meta.env.VITE_TEMPLATE
+  const USER = import.meta.env.VITE_USER
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  console.log(errors)
+
+  const sendEmail = (formData) => {
+    emailjs
+      .send(SERVICE, TEMPLATE, formData, USER)
+      .then(
+        (result) => {
+          alert('Tu correo se a enviado con exito')
+          console.log(result.user_name);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    reset();
+  };
+
+  return (
+    <><Styled.Section >
+      <Styled.Container >
+        <Styled.Title id='mensaje'>Escribe a mi Email:</Styled.Title>
+        <Styled.Form onSubmit={handleSubmit(sendEmail)}>
+
+          <Styled.Label htmlFor="name">Nombre:</Styled.Label>
+          <Styled.Input
+
+            name='user_name'
+            type='text'
+            placeholder="nombre"
+            {...register('user_name', {
+              required: {
+                value: true,
+                message: 'Nombre es requerido'
+              },
+              minLength: {
+                value: 2,
+                message: 'Nombre debe tener almenos 2 caracteres'
+              },
+              maxLength: {
+                value: 30,
+                message: 'Nombre  debe tener maximo  30 caracteres'
+              }
+            })} />
+          {
+            errors.user_name && <Styled.Span>{errors.user_name.message}</Styled.Span>
+          }
+
+
+          <Styled.Label htmlFor="email">Email:</Styled.Label>
+          <Styled.Input
+            name='user_email'
+            type='email'
+            placeholder="user@user.com"
+            {...register('user_email', {
+              required: {
+                value: true,
+                message: 'Email es requerido'
+              },
+              pattern: {
+                value: /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+                message: 'Email no es valido'
+              }
+            })} />
+          {
+            errors.user_email && <Styled.Span>{errors.user_email.message}</Styled.Span>
+          }
+
+          <Styled.Label htmlFor="asunto">Asunto:</Styled.Label>
+          <Styled.Input
+
+            name='user_asunto'
+            type='text'
+            placeholder="asunto"
+            {...register('user_asunto', {
+              required: {
+                value: true,
+                message: 'Asunto Requerido'
+              }
+            })} />
+          {
+            errors.user_asunto && <Styled.Span>{errors.user_asunto.message}</Styled.Span>
+          }
+
+          <Styled.Label htmlFor="message">Mensaje:</Styled.Label>
+          <Styled.Textatea
+            id='message'
+            name='message'
+            cols={'24'}
+            rows={`5`}
+            placeholder="mensaje"
+            {...register('message', {
+              required: {
+                value: true,
+                message: 'El mensaje es requerido '
+              }
+            })} />
+          {
+            errors.message && <Styled.Span>{errors.message.message}</Styled.Span>
+          }
+
+          <Styled.Button value={'send'}>Enviar</Styled.Button>
+        </Styled.Form>
+      </Styled.Container>
+
+    </Styled.Section>
+
+    </>
+
+  )
+
+}
+export default EmailContact
---
+++ Portfolio/src/components/Contact/styles.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+
+const Section = styled.section`
+display: flex;
+width: 100%;
+ margin-top: 20%;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin-block:10%
+`
+const Container = styled.div`
+margin-top: 10%;
+-webkit-box-shadow: 21px 17px 14px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 21px 17px 14px 0px rgba(0,0,0,0.75);
+box-shadow: 21px 17px 14px 0px rgba(0,0,0,0.75);
+border: 3px solid #ccc;
+padding: 25px;
+border-radius: 10px;
+background-color: #01454f;
+`
+const Form = styled.form`
+display: flex;
+flex-direction:column;
+ min-width: 600px;
+margin: 0 auto;
+
+  @media (max-width: 540px) {
+   min-width: 350px;
+  } 
+
+`
+const Title = styled.h1`
+font-size:2rem;
+color: white;
+text-align: left;
+`
+const Input = styled.input`
+width: 98%;
+margin-bottom: 20px;
+border: 3px solid #ccc;
+border-radius: 4px;
+`;
+
+const Textatea = styled.textarea`
+width: 98%;
+margin-bottom: 20px;
+border: 3px solid #ccc;
+border-radius: 4px;
+`
+const Button = styled.button`
+margin-top: 20px;
+margin-right: 25%;
+margin-left: 25%;
+text-align: center;
+border: 3px solid #ccc;
+border-radius: 4px;
+`
+const Label = styled.label`
+font-size: 20px;
+color: white;
+display: block;
+margin-bottom: 10px;
+`;
+const Span = styled.span`
+display:block;
+color: #d08383;
+font-size: x-small;
+margin-top:-15px;
+text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+`
+export default {
+    Section,
+    Form,
+    Input,
+    Title,
+    Textatea,
+    Button,
+    Label,
+    Span,
+    Container,
+
+
+}
