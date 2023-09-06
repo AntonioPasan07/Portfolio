3a6dc5895fab224fde2dc4c0d2b41f17b73fd4c6
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-09-06 17:06:32 +0200

    (chore), content for the project and skills card

---
+++ Portfolio/src/components/Project/index.jsx
@@ -0,0 +1,45 @@
+import Styled from '../Studies/styles'
+import { useState } from 'react';
+import data from '../../data.json'
+
+const Project = () => {
+
+  const skills = data.soft.skills
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Styled.BookContainer>
+      <Styled.Book isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}>
+        <Styled.BookInnerFront>
+          <Styled.Title id='proyectos'>Proyectos:</Styled.Title>
+          <Styled.CardProject>
+            {Object.keys(data.project).map((key) => (
+              <Styled.card2 key={key} style={{ backgroundImage: `url(${data.project[key].img})` }}>
+                <a href={data.project[key].link} target="_blank" rel="noopener noreferrer">
+                  <Styled.Pcard>{data.project[key].title}</Styled.Pcard>
+                </a>
+              </Styled.card2>
+            ))}
+          </Styled.CardProject>
+          <Styled.Click>Click</Styled.Click>
+        </Styled.BookInnerFront>
+        <Styled.BookInnerBack >
+          <Styled.Title>Soft Skills:</Styled.Title>
+          <Styled.ContainerSoft>
+            <Styled.Container1>
+              {skills.text1.map((skill, index) => (
+                <Styled.Texto1 key={index}>{skill}</Styled.Texto1>
+              ))}
+            </Styled.Container1>
+            <Styled.Container2>
+              {skills.text2.map((skill, index) => (
+                <Styled.Texto1 key={index}>{skill}</Styled.Texto1>
+              ))}
+            </Styled.Container2>
+          </Styled.ContainerSoft>
+          <Styled.Click>Click</Styled.Click>
+        </Styled.BookInnerBack>
+      </Styled.Book>
+    </Styled.BookContainer>
+  )
+}
+export default Project
